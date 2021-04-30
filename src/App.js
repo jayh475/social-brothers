d1ea2372b938c6c044056fcc4190fc71cb90a468
jayh475 <57130830+jayh475@users.noreply.github.com>
2021-04-30 23:33:44 +0200

    css tricks page is fully operational (except button: "meer laden")

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import {Component} from "react";
-import './App.css';
 import SocialBrothers from "./components/SocialBrothers";
 
 class App extends Component {
     render()
     {
         return (
-            <div className="App">
+            <div >
                 <SocialBrothers/>
-
             </div>
         );
     }
